b73f85df2041b2822d1abf5fb869054971e0a374
Jakob Hansen <jakob.hansen@hotmail.no>
2025-04-22 15:05:57 +0200

    Try deadzone only when pressing key

--- users/colemak_jkl/code.c
+++ users/colemak_jkl/code.c
@@ -37,8 +37,21 @@ combo_t key_combos[] = {
 };
 // clang-format on
 
+bool mouse1_pressed = false;
+bool mouse2_pressed = false;
+bool mouse3_pressed = false;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+        case KC_BTN1:
+            mouse1_pressed = record->event.pressed;
+            break;
+        case KC_BTN2:
+            mouse2_pressed = record->event.pressed;
+            break;
+        case KC_BTN3:
+            mouse3_pressed = record->event.pressed;
+            break;
         case LALT_T(LGUI(KC_4)):
             if (record->tap.count && record->event.pressed) {
                 tap_code16(LGUI(KC_4));
@@ -63,6 +76,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 #ifdef POINTING_DEVICE_ENABLE
     #include "pointing_device.h"
+    #define SCROLL_DIVISOR_H 20.0
+    #define SCROLL_DIVISOR_V 20.0
+    #define DEADZONE 2
+
 // Hack since this is not exposed by driver code
 void cirque_pinnacle_enable_cursor_glide(bool enable);
 
@@ -70,16 +87,9 @@ void cirque_pinnacle_enable_cursor_glide(bool enable);
 int8_t move_accumulated_x = 0;
 int8_t move_accumulated_y = 0;
 
-    // Modify these values to adjust the scrolling speed
-    #define SCROLL_DIVISOR_H 20.0
-    #define SCROLL_DIVISOR_V 20.0
-
 // Variables to store accumulated scroll values
 float scroll_accumulated_h = 0;
 float scroll_accumulated_v = 0;
-
-    // Cursor movement
-    #define DEADZONE 2 // Set this to the number of counts to ignore
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     pointing_device_task_auto_mouse(mouse_report);
@@ -106,28 +116,32 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
         return mouse_report;
     }
     cirque_pinnacle_enable_cursor_glide(false);
-    // Accumulate movement
-    move_accumulated_x += mouse_report.x;
-    move_accumulated_y += mouse_report.y;
 
-    int8_t move_x = move_accumulated_x;
-    int8_t move_y = move_accumulated_y;
+    // Apply deadzone if any button is pressed
+    if (mouse1_pressed || mouse2_pressed || mouse3_pressed) {
+        // Accumulate movement
+        move_accumulated_x += mouse_report.x;
+        move_accumulated_y += mouse_report.y;
 
-    move_accumulated_x -= move_x;
-    move_accumulated_y -= move_y;
+        int8_t move_x = move_accumulated_x;
+        int8_t move_y = move_accumulated_y;
 
-    // Apply deadzone after accumulation
-    if (move_x > -DEADZONE && move_x < DEADZONE) {
-        move_x = 0;
-        move_accumulated_x += move_x;
+        move_accumulated_x -= move_x;
+        move_accumulated_y -= move_y;
+
+        // Apply deadzone after accumulation
+        if (move_x > -DEADZONE && move_x < DEADZONE) {
+            move_x = 0;
+            move_accumulated_x += move_x;
+        }
+        if (move_y > -DEADZONE && move_y < DEADZONE) {
+            move_y = 0;
+            move_accumulated_y += move_y;
+        }
+
+        mouse_report.x = move_x;
+        mouse_report.y = move_y;
     }
-    if (move_y > -DEADZONE && move_y < DEADZONE) {
-        move_y = 0;
-        move_accumulated_y += move_y;
-    }
-
-    mouse_report.x = move_x;
-    mouse_report.y = move_y;
 
     return mouse_report;
 }
