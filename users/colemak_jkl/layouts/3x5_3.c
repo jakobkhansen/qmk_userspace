d66da391dea969d63f7b223bc2fb5a5c69ebe00f
Jakob Hansen <jakob.hansen@hotmail.no>
2025-05-06 11:47:29 +0200

    Move middle mouse button

--- users/colemak_jkl/layouts/3x5_3.c
+++ users/colemak_jkl/layouts/3x5_3.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MOUSE] = LAYOUT_split_3x5_3( \
         KC_Q,          KC_W,          KC_F,             KC_P,             KC_B,            KC_Y,  KC_J,          KC_K,          KC_L,          KC_SCLN,       \
         LCTL_T(KC_A),  LALT_T(KC_R),  LGUI_T(KC_S),     LSFT_T(KC_T),     KC_G,            KC_H,  RSFT_T(KC_N),  LGUI_T(KC_E),  LALT_T(KC_O),  LCTL_T(KC_I),  \
-        LT(FUN,KC_Z),  KC_X,          KC_C,             KC_D,             KC_BTN3,         KC_M,  KC_U,          KC_COMMA,      KC_DOT,        KC_MINUS,      \
+        LT(FUN,KC_Z),  KC_X,          KC_C,             KC_BTN3,          KC_V,         KC_M,  KC_U,          KC_COMMA,      KC_DOT,        KC_MINUS,      \
         XXX,           KC_BTN1,       LT(NAV,KC_BTN2),  LT(NUM,KC_BSPC),  LT(SYM,KC_ENT),  XXX    \
 
     ),
